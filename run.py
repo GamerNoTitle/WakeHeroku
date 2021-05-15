bbad087ede398868ddfe26cb50d4315faf7f5433
イレイナ❤ <69030283+ElainaMoe@users.noreply.github.com>
2021-05-15 10:37:36 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,12 +2,8 @@ import requests
 import sys
 import time
 
-urls = ['https://heroku-us.pesy.workers.dev']
+urls = ['https://gamernotitle-heroku-us.pesy.workers.dev','http://wikijs.bili33.top','https://share4nothing.ml','https://rss.bili33.top']
 
-if (len(sys.argv) >= 2):
-    urls = ['https://gamernotitle-heroku-us.pesy.workers.dev','http://wikijs.bili33.top','https://share4nothing.ml','https://rss.bili33.top']
-else:
-    urls = ['https://bili33.top']
 for i in range(0, len(urls)):
     req = requests.get(urls[i])
     print(f'No.{i} Wakeup Status: ', req, time.strftime(
