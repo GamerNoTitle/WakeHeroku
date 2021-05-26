e0bc8c7c54ba7cede74cc26c965d723048d382d7
イレイナ❤ <69030283+ElainaMoe@users.noreply.github.com>
2021-05-26 18:01:32 +0800

    Update run.py

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ urls = ['https://gamernotitle-heroku-us.pesy.workers.dev','http://wikijs.bili33.
 
 for i in range(0, len(urls)):
     req = requests.get(urls[i])
-    print(f'No.{i} Wakeup Status: ', req, time.strftime(
+    print(f'No.{i} Wakeup Status: {urls[i]} ', req, time.strftime(
         '%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
