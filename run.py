eca5cb9edf6ccc3b3d90c583d185cccd8ee8c164
イレイナ❤ <69030283+ElainaMoe@users.noreply.github.com>
2022-02-21 10:30:54 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import requests
 import sys
 import time
 
-urls = ['https://share4nothing.ml','https://rss.bili33.top','https://zfile.share4nothing.ml']
+urls = ['https://ninym.top']
 
 for i in range(0, len(urls)):
     req = requests.get(urls[i])
